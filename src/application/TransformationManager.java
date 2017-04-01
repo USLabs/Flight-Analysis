a7e70872b6f95f7268d897f768b4d22e069135c2
Umang Saxena <umang.saxena@sjsu.edu>
2017-04-01 02:29:29 -0700

    Analysis Code

---
+++ src/application/TransformationManager.java
@@ -0,0 +1,34 @@
+package application;
+
+import transformation.*;
+import utility.*;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaRDDLike;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
+
+public class TransformationManager {
+
+    public void calculateMaxTakeOff(JavaSparkContext ctx, String flightData, int partitions) {
+        // transformers initialization
+        ValidRecordFilter validRecordFilter = new ValidRecordFilter();
+        RawToPairTransformer rawToPairTransformer = new RawToPairTransformer();
+        OccurenceSummarizer occurenceSummarizer = new OccurenceSummarizer();
+        OriginTupleComparator originTupleComparator = new OriginTupleComparator();
+
+        // RDD transformations
+        JavaRDD<String> lines = ctx.textFile(flightData, partitions);
+        JavaRDD<String> validRecords = lines.filter(validRecordFilter);
+        JavaPairRDD<String, Integer> flightOrigins = validRecords.mapToPair(rawToPairTransformer);
+        JavaPairRDD<String, Integer> occurencesByKey = flightOrigins.reduceByKey(occurenceSummarizer);
+        // workaround, otherwise .max function could not be invoked (SPARK-3266)
+        Tuple2<String, Integer> max = ((JavaRDDLike<Tuple2<String, Integer>, ?>) occurencesByKey)
+                .max(originTupleComparator);
+
+        // print result
+        System.out.println("\nMost of the airplanes took off from " + max._1 + ", " + max._2 + " times.");
+    }
+}
